a05757693d5485f9be2900c68e590c54ff9a28b0
foodnoodle <135312352+foodnoodle@users.noreply.github.com>
2025-12-09 23:45:09 +0800

    Update naaim_index.py

--- naaim_index.py
+++ naaim_index.py
@@ -10,22 +10,29 @@ from selenium.webdriver.chrome.options import Options
 def fetch_naaim_exposure_index():
     """爬取 NAAIM 曝險指數"""
     TARGET_URL = "https://naaim.org/programs/naaim-exposure-index/"
+    
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # --- 關鍵修改：加入 Headless 設定 ---
+    chrome_options.add_argument("--headless") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
+    # ---------------------------------
+    
     chrome_options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36')
+    
     service = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service, options=chrome_options)
+    
     try:
         driver.get(TARGET_URL)
         wait = WebDriverWait(driver, 10)
+        # 等待元素出現
         wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div#brxe-ymwzia.brxe-shortcode")))
+        
         naaim_element = driver.find_element(By.CSS_SELECTOR, "div#brxe-ymwzia.brxe-shortcode")
         naaim_value = naaim_element.text
         return naaim_value
     except Exception as e:
         return f"抓取過程中發生錯誤: {e}"
     finally:
-
         driver.quit()
